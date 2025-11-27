12629f700fb691169feac4b5689c9f69f38ab806
MaratIl <117185885+MaratIl@users.noreply.github.com>
2025-11-27 10:24:51 +0300

    Row cards complete

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-import styles from "../shared/style/Homepage.module.css";
 import { Col, Container, Row } from "react-bootstrap";
 import axios from "axios";
 import PropertyCard from "../entities/PropertyCard";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 
-function HomePage({ user }) {
+function HomePage() {
   const [properties, setProperties] = useState([]);
 
   useEffect(() => {
@@ -16,29 +15,33 @@ function HomePage({ user }) {
   console.log(properties);
   return (
     <div>
-      {properties.map((property) => {
-        return (
-          <>
-            <Row xs={1} md={2} className="g-4">
-              {Array.from({ length: 4 }).map((_, idx) => (
-                <Col key={idx}>
-                  <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                      <Card.Title>{property.type}</Card.Title>
-                      <Card.Text>
-                        This is a longer card with supporting text below as a
-                        natural lead-in to additional content. This content is a
-                        little bit longer.
-                      </Card.Text>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              ))}
-            </Row>
-          </>
-        );
-      })}
+      <Container>
+        <Row>
+          {properties.map((property) => {
+            return (
+              <>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src="holder.js/100px180?text=Image cap"
+                  />
+                  <Card.Body>
+                    <Card.Title>{property.type}</Card.Title>
+                    <Card.Text>{property.descriptions}</Card.Text>
+                  </Card.Body>
+                  <ListGroup className="list-group-flush">
+                    <ListGroup.Item> Цена: {property.price} руб</ListGroup.Item>
+                    <ListGroup.Item>Адрес: {property.addres}</ListGroup.Item>
+                  </ListGroup>
+                  <Card.Body>
+                    <Card.Link href="#">Написать</Card.Link>
+                  </Card.Body>
+                </Card>
+              </>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }

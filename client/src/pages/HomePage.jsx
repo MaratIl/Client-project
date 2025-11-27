4a79f2f33106ebf53032c213dcb357c08ef70c80
MaratIl <117185885+MaratIl@users.noreply.github.com>
2025-11-27 08:51:03 +0300

    Made map property

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
-import "../shared/style/Homepage.module.css";
+import styles from "../shared/style/Homepage.module.css";
 import { Col, Container, Row } from "react-bootstrap";
 import axios from "axios";
+import PropertyCard from "../entities/PropertyCard";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
 function HomePage({ user }) {
   const [properties, setProperties] = useState([]);
@@ -11,19 +14,32 @@ function HomePage({ user }) {
   }, []);
 
   console.log(properties);
-//как добавить стили для дива
   return (
-    <>
-      {properties.map((el) => {
+    <div>
+      {properties.map((property) => {
         return (
           <>
-            <div>{el.type}</div>
-            <div>{el.price}</div>
-            <div>{el.addres}</div>
+            <Row xs={1} md={2} className="g-4">
+              {Array.from({ length: 4 }).map((_, idx) => (
+                <Col key={idx}>
+                  <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                      <Card.Title>{property.type}</Card.Title>
+                      <Card.Text>
+                        This is a longer card with supporting text below as a
+                        natural lead-in to additional content. This content is a
+                        little bit longer.
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
           </>
-        )
+        );
       })}
-    </>
+    </div>
   );
 }
 
